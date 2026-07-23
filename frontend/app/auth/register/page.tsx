cf682b833b5daa0fc5871804d74118923f39b1d4
NITIN GUPTA <88649588+nitin28061999@users.noreply.github.com>
2026-07-23 21:55:19 +0530

    Create page.tsx

---
+++ frontend/app/auth/register/page.tsx
@@ -0,0 +1,35 @@
+﻿"use client";
+
+export default function RegisterPage() {
+  return (
+    <div className="bg-white p-8 rounded shadow w-96">
+
+      <h1 className="text-2xl font-bold mb-6">
+        Create Account
+      </h1>
+
+      <input
+        className="border w-full p-2 mb-3"
+        placeholder="Full Name"
+      />
+
+      <input
+        className="border w-full p-2 mb-3"
+        placeholder="Email"
+      />
+
+      <input
+        className="border w-full p-2 mb-3"
+        type="password"
+        placeholder="Password"
+      />
+
+      <button
+        className="bg-green-600 text-white w-full p-2 rounded"
+      >
+        Register
+      </button>
+
+    </div>
+  );
+}
